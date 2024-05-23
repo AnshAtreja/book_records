46e66b27ec64df5d7d0d22099e92dca8099cb1f4
AnshAtreja <anshatreja2002@gmail.com>
2024-05-23 23:52:55 +0530

    Helper api functions

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -3,33 +3,33 @@
 import axios from 'axios';
 
 // Helper function to fetch book details
-const fetchBookDetails = async (title) => {
-    try {
-        const response = await axios.get(`https://openlibrary.org/search.json?q=${encodeURIComponent(title)}`);
-        const book = response.data.docs[0];
-        return {
-            ratings_average: book.rating || 'N/A'
-        };
-    } catch (error) {
-        console.error(`Error fetching book details for title "${title}":`, error);
-        return { ratings_average: 'N/A' };
-    }
-};
+// const fetchBookDetails = async (title) => {
+//     try {
+//         const response = await axios.get(`https://openlibrary.org/search.json?q=${encodeURIComponent(title)}`);
+//         const book = response.data.docs[0];
+//         return {
+//             ratings_average: book.rating || 'N/A'
+//         };
+//     } catch (error) {
+//         console.error(`Error fetching book details for title "${title}":`, error);
+//         return { ratings_average: 'N/A' };
+//     }
+// };
 
 // Helper function to fetch author details
-const fetchAuthorDetails = async (authorName) => {
-    try {
-        const response = await axios.get(`https://openlibrary.org/search/authors.json?q=${encodeURIComponent(authorName)}`);
-        const author = response.data.docs[0];
-        return {
-            author_birth_date: author.birth_date || 'N/A',
-            author_top_work: author.top_work || 'N/A'
-        };
-    } catch (error) {
-        console.error(`Error fetching author details for author "${authorName}":`, error);
-        return { author_birth_date: 'N/A', author_top_work: 'N/A' };
-    }
-};
+// const fetchAuthorDetails = async (authorName) => {
+//     try {
+//         const response = await axios.get(`https://openlibrary.org/search/authors.json?q=${encodeURIComponent(authorName)}`);
+//         const author = response.data.docs[0];
+//         return {
+//             author_birth_date: author.birth_date || 'N/A',
+//             author_top_work: author.top_work || 'N/A'
+//         };
+//     } catch (error) {
+//         console.error(`Error fetching author details for author "${authorName}":`, error);
+//         return { author_birth_date: 'N/A', author_top_work: 'N/A' };
+//     }
+// };
 
 export const fetchBooks = async (limit = 50) => {
     const response = await axios.get(`https://openlibrary.org/subjects/science_fiction.json?limit=50`);
